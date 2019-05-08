655e04414639ab2e598d9c41f1fb74ea1b03c02e
Peadar Kelly <peadark@kainos.com>
2019-05-08 17:02:28 +0100

    Extended inspections request to include dates

--- dist/interfaces/getInspectionsRequest.d.ts
+++ dist/interfaces/getInspectionsRequest.d.ts
@@ -3,4 +3,6 @@ import { ReportingRequest } from './reportingRequest';
 export interface GetInspectionsRequest extends ReportingRequest {
     inspection_response_type?: InspectionResponseType[];
     sort_column?: InspectionSortColumn;
+    start_date?: Date;
+    end_date?: Date;
 }
--- src/interfaces/getInspectionsRequest.ts
+++ src/interfaces/getInspectionsRequest.ts
@@ -4,4 +4,6 @@ import { ReportingRequest } from './reportingRequest'
 export interface GetInspectionsRequest extends ReportingRequest {
   inspection_response_type?: InspectionResponseType[]
   sort_column?: InspectionSortColumn
+  start_date?: Date
+  end_date?: Date
 }
