379bf5cab1cb41591f116a7544de528b139871c0
shaung <s.ganley@kainos.com>
2019-06-13 09:13:14 +0100

    adding submission date filter fields to get permit request

--- dist/interfaces/getPermitsRequest.d.ts
+++ dist/interfaces/getPermitsRequest.d.ts
@@ -4,6 +4,8 @@ export interface GetPermitsRequest extends ReportingRequest {
     status?: PermitStatus[];
     start_date?: Date;
     end_date?: Date;
+    submission_from_date?: Date;
+    submission_to_date?: Date;
     active_permit_only?: boolean;
     sort_column?: PermitSortColumn;
 }
