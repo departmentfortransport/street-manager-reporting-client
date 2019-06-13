0366d6dab79cdfcc7166574159b66626635ef57b
shaung <s.ganley@kainos.com>
2019-06-13 11:46:23 +0100

    updating names for consistency

--- src/interfaces/getPermitsRequest.ts
+++ src/interfaces/getPermitsRequest.ts
@@ -5,8 +5,8 @@ export interface GetPermitsRequest extends ReportingRequest {
   status?: PermitStatus[]
   start_date?: Date
   end_date?: Date
-  start_application_submission_date?: Date
-  end_application_submission_date?: Date
+  start_date_created?: Date
+  end_date_created?: Date
   active_permit_only?: boolean
   sort_column?: PermitSortColumn
 }
