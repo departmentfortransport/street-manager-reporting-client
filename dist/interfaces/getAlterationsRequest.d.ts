23f208d02ebfdf6e5e438dd20d24e99e8facc69f
Peadar Kelly <peadark@kainos.com>
2019-06-26 13:37:31 +0100

    Addde work category filtering to alterations

--- dist/interfaces/getAlterationsRequest.d.ts
+++ dist/interfaces/getAlterationsRequest.d.ts
@@ -1,6 +1,7 @@
-import { AlterationSortColumn } from './referenceTypes';
+import { AlterationSortColumn, WorkCategory } from './referenceTypes';
 import { ReportingRequest } from './reportingRequest';
 export interface GetAlterationsRequest extends ReportingRequest {
+    work_category?: WorkCategory[];
     start_date_created?: Date;
     end_date_created?: Date;
     sort_column?: AlterationSortColumn;
