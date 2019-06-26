4e580655e360da694a81585820717609d83edf3c
Peadar Kelly <peadark@kainos.com>
2019-06-26 15:48:04 +0100

    Added work status filtering to permits and alterations and alteration status filtering to alterations

--- dist/interfaces/getAlterationsRequest.d.ts
+++ dist/interfaces/getAlterationsRequest.d.ts
@@ -1,6 +1,8 @@
-import { AlterationSortColumn, WorkCategory } from './referenceTypes';
+import { AlterationSortColumn, AlterationStatus, WorkStatus, WorkCategory } from './referenceTypes';
 import { ReportingRequest } from './reportingRequest';
 export interface GetAlterationsRequest extends ReportingRequest {
+    alteration_status?: AlterationStatus[];
+    work_status?: WorkStatus[];
     work_category?: WorkCategory[];
     start_date_created?: Date;
     end_date_created?: Date;
