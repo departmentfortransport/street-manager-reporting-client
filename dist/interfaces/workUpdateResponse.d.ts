b0aac9334867051f7d78b753d3eab682ec026b6f
nathanaelh <nathanaelh@kainos.com>
2020-02-18 11:51:37 +0000

    SM-4733 Updated work update response

--- dist/interfaces/workUpdateResponse.d.ts
+++ dist/interfaces/workUpdateResponse.d.ts
@@ -2,4 +2,5 @@ export interface WorkUpdateResponse {
     work_reference_number: string;
     update_date_time: Date;
     update_id: number;
+    event_type: string;
 }
--- src/interfaces/workUpdateResponse.ts
+++ src/interfaces/workUpdateResponse.ts
@@ -1,5 +1,6 @@
 export interface WorkUpdateResponse {
   work_reference_number: string
   update_date_time: Date
-  update_id: number
+  update_id: number,
+  event_type: string
 }
