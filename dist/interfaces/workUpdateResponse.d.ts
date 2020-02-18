4de37adc427306a16719d6f04ce6cc3714e48f7d
nathanaelh <nathanaelh@kainos.com>
2020-02-18 13:14:24 +0000

    SM-4733 added redAuditEventType

--- dist/interfaces/workUpdateResponse.d.ts
+++ dist/interfaces/workUpdateResponse.d.ts
@@ -1,6 +1,7 @@
+import { AuditEventType } from './referenceTypes';
 export interface WorkUpdateResponse {
     work_reference_number: string;
     update_date_time: Date;
     update_id: number;
-    event_type: string;
+    event_type: AuditEventType;
 }
