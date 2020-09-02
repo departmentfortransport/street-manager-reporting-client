98de75272ecddc51a963f88fe479c46bbf4fc331
Catherine <c.paul@kainos.com>
2020-09-02 11:00:05 +0100

    SM-6145 - add additional inspection filters params

--- dist/interfaces/getInspectionsRequest.d.ts
+++ dist/interfaces/getInspectionsRequest.d.ts
@@ -1,8 +1,12 @@
-import { InspectionResponseType, InspectionSortColumn } from './referenceTypes';
+import { InspectionResponseType, InspectionSortColumn, InspectionType, InspectionOutcome } from './referenceTypes';
 import { ReportingRequest } from './reportingRequest';
 export interface GetInspectionsRequest extends ReportingRequest {
     inspection_response_type?: InspectionResponseType[];
     sort_column?: InspectionSortColumn;
     start_date?: Date;
     end_date?: Date;
+    inspection_type?: InspectionType[];
+    inspection_outcome?: InspectionOutcome[];
+    start_date_created?: Date;
+    end_date_created?: Date;
 }
