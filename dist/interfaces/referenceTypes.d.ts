cba0f48ad5b6015e1da851b0d935f65dd165fb77
Catherine <c.paul@kainos.com>
2020-10-14 15:29:36 +0100

    SM-5455 - add permit_granted_with_duration_challenge audit event

--- dist/interfaces/referenceTypes.d.ts
+++ dist/interfaces/referenceTypes.d.ts
@@ -369,7 +369,8 @@ export declare enum AuditEventType {
     excavation_carried_out_updated = "excavation_carried_out_updated",
     section_81_linked_to_permit = "section_81_linked_to_permit",
     section_81_unlinked_from_permit = "section_81_unlinked_from_permit",
-    section_81_reassigned = "section_81_reassigned"
+    section_81_reassigned = "section_81_reassigned",
+    permit_granted_with_duration_challenge = "permit_granted_with_duration_challenge"
 }
 export declare enum AuditObjectType {
     PERMIT = "permit",
